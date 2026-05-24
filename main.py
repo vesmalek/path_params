f486cc73099a8070d950d7daa636196358a29dac
Ismail Malik <vesmalick@gmail.com>
2026-05-24 19:11:00 +0300

    Using type hints into path params

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello friend!")
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/products/{product_id}")
+async def root(product_id: int):
+    return {"product_id": product_id}
+
+@app.get("/")
+async def root():
+    return {"message": "My API is working"}
