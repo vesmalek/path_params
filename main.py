bd67ac27c2ebc5a7a3fdd694927b963f830eac0a
Ismail Malik <vesmalick@gmail.com>
2026-05-24 19:21:06 +0300

    Learned to specify values with Enum

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
+from enum import Enum
 from fastapi import FastAPI
+
+class PlanName(str, Enum):
+    basic = "basic"
+    pro = "pro"
+    enterprise = "enterprise"
 
 app = FastAPI()
 
@@ -9,3 +15,11 @@ async def root(product_id: int):
 @app.get("/")
 async def root():
     return {"message": "My API is working"}
+
+@app.get("/plans/{plan_name}")
+async def root(plan_name: PlanName):
+    if plan_name is PlanName.basic:
+        return {"plan_name": plan_name, "price": "$9/month"}
+    if plan_name is PlanName.pro:
+        return {"plan_name": plan_name, "price": "$29/month"}
+    return {"plan_name": plan_name, "price": "$50/month"}
